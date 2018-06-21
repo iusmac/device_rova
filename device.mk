031ddb914a2e7e70de8e5d4e490f8c5c13f8853e
Michael Wright <michaelwr@google.com>
2018-06-21 02:38:28 +0100

    rova: Update libperfmgr HAL to version 1.3
    This adds support for the EXPENSIVE_RENDERING power hint.
    
    BUG: 110112323
    Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
          Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
    Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -322,7 +322,7 @@ TARGET_BOARD_SUFFIX := _64
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_rova-libperfmgr \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.3.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
