6cd93615b8fcc9b2aa61e7024693c61888f562c9
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    rova: Panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,7 +135,7 @@ endif
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78b000
-BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
