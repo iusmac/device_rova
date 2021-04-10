c8d25f0ab12dfbbbeef61584713186c61c674468
AdarshGrewal <adarsh@aosip.dev>
2021-04-10 11:47:51 +0530

    rova: sepolicy: include device private sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ VENDOR_SECURITY_PATCH := 2018-07-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
