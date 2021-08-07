ce49d21135699e972cbd010f1bb9ae9da7153f88
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-07 23:18:11 +0800

    rova: audio: Import base mixer_paths from LA.UM.8.6.2.r1-08100-89xx.0

--- device.mk
+++ device.mk
@@ -8,6 +8,14 @@
 $(call inherit-product, device/xiaomi/rova-common/rova.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/rolex_mixer_paths_qrd_sku2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/rolex_mixer_paths_qrd_sku2.xml \
+    $(LOCAL_PATH)/audio/riva_mixer_paths_qrd_sku2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/riva_mixer_paths_qrd_sku2.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/blank.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku2.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.xiaomi.device.rc \
