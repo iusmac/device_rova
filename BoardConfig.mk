5f52edb38535b93979ce68787e80f480344bae52
daniml3 <danimoral1001@gmail.com>
2021-08-12 15:18:39 +0000

    rova: Switch to SYSTEM_EXT_*_SEPOLICY_DIRS
    * BOARD_PLAT_*_SEPOLICY_DIR has been deprecated
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ VENDOR_SECURITY_PATCH := 2020-02-28
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
