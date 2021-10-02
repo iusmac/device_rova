boedhack99/android_device_xiaomi_rova

rova: Inherit from common tree
Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

BoardConfig.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common msm8937-common
+include device/xiaomi/msm8937-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/rova
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := rolex,riva,rova
+
+# Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-07-01
+
+# Inherit from the proprietary version
+include vendor/xiaomi/rova/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,16 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from msm8937-common
+$(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/rova/rova-vendor.mk)

extract-files.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=rova
+export DEVICE_COMMON=msm8937-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=rova
+export DEVICE_COMMON=msm8937-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

iusmac/device_rova

BoardConfig.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common rova-common
+include device/xiaomi/rova-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/rova
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := rolex,riva,rova
+
+# Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-07-01
+
+# Inherit from the proprietary version
+include vendor/xiaomi/rova/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,16 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from rova-common
+$(call inherit-product, device/xiaomi/rova-common/rova.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/rova/rova-vendor.mk)

extract-files.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=rova
+export DEVICE_SPECIFIED_COMMON=rova-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_SPECIFIED_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=rova
+export DEVICE_SPECIFIED_COMMON=rova-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_SPECIFIED_COMMON}/setup-makefiles.sh" "$@"