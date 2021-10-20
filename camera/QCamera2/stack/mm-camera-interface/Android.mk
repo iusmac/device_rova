7d2b239e22bdb9098a0f28793f711777ab5b37af
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-20 02:51:01 +0000

    rova: camera: Fix compilation on S
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -52,7 +52,7 @@ ifneq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 17 ))" )))
   LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/un.h
 endif
 
-LOCAL_CFLAGS += -Wall -Wextra -Wno-error -Wno-implicit-function-declaration
+LOCAL_CFLAGS += -Wall -Wextra -Wno-error -Wno-implicit-function-declaration -Wno-compound-token-split-by-macro
 
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
