d6c5a8a8b0860785d7f2cf56964af0cdd9cf48c5
ItoRenz <ridwan.ri214@gmail.com>
2021-11-18 01:45:14 +0700

    rova: overlay: clean up some overlays
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- device.mk
+++ device.mk
@@ -11,10 +11,6 @@ DEVICE_PACKAGE_OVERLAYS +=  \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    xiaomi_rolex_overlay \
-    xiaomi_riva_overlay
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
