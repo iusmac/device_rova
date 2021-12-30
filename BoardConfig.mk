bb4d88d866e38500712e4c2a2f3c0275f997b7c8
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-12-30 11:13:10 +0800

    rova: Export LLVM=1 to Kernel build
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ TARGET_KERNEL_CONFIG := rova_defconfig
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    LLVM=1 \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Media
