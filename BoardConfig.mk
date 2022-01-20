b6a07cde4109ad804d10cdc0c8a644c1548daf1b
Etienne Ruffieux <eruffieux@google.com>
2022-01-20 21:18:07 +0000

    rova: Added new vendors for Bluetooth config migration
    [iusmac]: added {.enabled} postfix
    
    Tag: #feature
    Test: manual
    Bug: 211570675
    Change-Id: Ib69dd91027c9579f2fc12099d973c993f2c073cd
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/dt2w"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
