0ac4321d540c6cb089ee26a1a0af8f7c8da1ff0f
iusmac <iusico.maxim@libero.it>
2022-01-26 15:38:29 +0100

    rova: Move overlay-lineage to lineage related .mk file
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -8,8 +8,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS +=  \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
