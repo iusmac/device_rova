a364586f0f1b28da2590b2ec61ec2f2751c002b9
iusmac <iusico.maxim@libero.it>
2022-01-27 12:32:33 +0100

    rova: Implement disable_keys feature into vendor.lineage.touch interface
    This implementation adds support for disabling touchscreen keys from
    userspace if the ROM has this implemented feature.
    
    * the support is added at kernel level only for the following
      touchscreen drivers:
      - FT5346 (used by riva)
      - GT9XX_V2.8 (used by rolex)
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -378,6 +378,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_rova
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
