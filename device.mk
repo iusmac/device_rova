547b22126fdff9f87ec78ee1e665ceffb934b286
iusmac <iusico.maxim@libero.it>
2022-03-13 12:59:34 +0100

    rova: Uprev GPS HIDL to 1.2
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -172,7 +172,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@1.2.vendor \
     android.hardware.gnss@2.1.vendor \
     android.hardware.gnss@3.0.vendor
 
