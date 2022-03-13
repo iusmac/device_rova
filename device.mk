dae1814837933df5b47b58987224143b25ed7f0b
iusmac <iusico.maxim@libero.it>
2022-03-13 13:10:20 +0100

    rova: Provide @3.0-impl/@4.0-impl of android.hardware.graphics.allocator
    02-18 23:59:21.634   636   724 D BootAnimation: BootAnimationShownTiming start time: 21205ms
    02-18 23:59:21.710   607   607 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.mapper@4.0::IMapper/default in either framework or device VINTF manifest.
    02-18 23:59:21.710   636   724 I Gralloc4: mapper 4.x is not supported
    02-18 23:59:21.710   607   607 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.mapper@3.0::IMapper/default in either framework or device VINTF manifest.
    02-18 23:59:21.711   636   724 W Gralloc3: mapper 3.x is not supported
    02-18 23:59:21.723   636   724 I Gralloc2: Adding additional valid usage bits: 0x202000
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -123,6 +123,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
 	gralloc.msm8937
 
