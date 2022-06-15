d9a1aa50577cc9c955185fb082a9803564351d4f
iusmac <iusico.maxim@libero.it>
2022-06-15 14:53:06 +0200

    rova: fstab: Switch to File-Based Encryption (FBE)
    * move folks to FBE as FDE is obsolete and gets frequently broken on
      recent ROMs, also it will be completely removed in Android T release.
    * also required by crDroid new rules since we're preparing to go Official
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -213,10 +213,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     libshim_imsvideocodec \
