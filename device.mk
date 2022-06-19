05945b88cce93c2f4939886b2c5a84689e4f917e
iusmac <iusico.maxim@libero.it>
2022-06-19 10:26:25 +0200

    rova: Import QTI libdisplayconfig & build vendor.display.config@2.0
    E QSEECOMD: : Init dlopen(libops.so, RLTD_NOW) is failed.... dlopen failed: library "libdisplayconfig.qti.so" not found: needed by /vendor/lib64/libops.so in namespace (default)
    
    E QSEECOMD: : Init dlopen(libops.so, RLTD_NOW) is failed.... dlopen failed: library "vendor.display.config@2.0.so" not found: needed by /vendor/lib64/libdisplayconfig.qti.so in namespace (default)
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libgenlock \
     libtinyxml \
-    vendor.display.config@1.0.vendor
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0.vendor
 
 # XiaomiParts
 PRODUCT_PACKAGES += \
