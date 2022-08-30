9dba116926d5cf26119be23665dc4d2927ece683
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-31 01:29:44 +0800

    rova: thermal: Disable BCL type of sensors
    These thermal zones are not giving temp value on our devices
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- thermal/thermalConfig.cpp
+++ thermal/thermalConfig.cpp
@@ -959,6 +959,7 @@ namespace implementation {
 		true,
 	};
 
+/*
 	std::vector<struct target_therm_cfg> bcl_conf = {
 		{
 			TemperatureType::BCL_VOLTAGE,
@@ -979,6 +980,7 @@ namespace implementation {
 			true,
 		},
 	};
+*/
 
 	const std::unordered_map<int, std::vector<struct target_therm_cfg>>
 		msm_soc_map = {
@@ -1087,9 +1089,11 @@ namespace implementation {
 		}
 
 		thermalConfig.push_back(bat_conf);
+/*
 		if (!bcl_defined)
 			thermalConfig.insert(thermalConfig.end(),
 				bcl_conf.begin(), bcl_conf.end());
+*/
 		LOG(DEBUG) << "Total sensors:" << thermalConfig.size();
 	}
 }  // namespace implementation
