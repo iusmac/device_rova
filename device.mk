9e139d1f8fdf92ec2b7966d5cc57971589e36746
Bruno Martins <bgcngm@gmail.com>
2022-10-02 23:50:32 +0200

    rova: Drop component overrides
    This is no longer needed as of Android 13.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -70,9 +70,6 @@ PRODUCT_PACKAGES += \
     libspkrprot
 
 # Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
