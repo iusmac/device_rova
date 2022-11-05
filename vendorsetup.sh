7f31917cc7882b3b8354fe2249d4d8aaafa98535
iusmac <iusico.maxim@libero.it>
2022-11-05 14:28:56 +0100

    rova: vendorsetup: Force recreate system/build.prop on dirty builds
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,11 @@
 
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
+# Remove the following intermediate buildinfo.prop file to trigger
+# gen_from_buildinfo_sh rule in build/core/sysprop.mk. This will populate
+# system/build.prop file with fresh infos when making "dirty" build.
+rm -vf $OUT_DIR/target/product/$lunch_device/obj/PACKAGING/system_build_prop_intermediates/buildinfo.prop
+
 # ************************************
 # Fix Bluetooth calling on legacy SOCs
 # ************************************
