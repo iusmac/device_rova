f045ed65ed3477a7af068714d1a3a10635416bce
Jprimero15 <jprimero15@aospa.co>
2023-09-17 22:59:20 +0800

    rova: Use TARGET_COPY_OUT_SYSTEM instead of hardcoded path
    * better
    
    Change-Id: Ic572d181fc169a663e27127957f879e6a9dce641
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- device.mk
+++ device.mk
@@ -231,9 +231,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles_8937.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media/media_profiles_8937.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles.xml \
     $(LOCAL_PATH)/media/media_profiles_8937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
-    $(LOCAL_PATH)/media/media_profiles_8956.xml:system/etc/media_profiles_8956.xml \
+    $(LOCAL_PATH)/media/media_profiles_8956.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_8956.xml \
     $(LOCAL_PATH)/media/media_profiles_8956.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_8956.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
