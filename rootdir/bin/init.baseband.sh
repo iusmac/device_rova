ea75df0dff3ba8e8f690c4fada4f014871541fc4
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-16 02:42:33 +0800

    rova: Parse modem version string from /sys/devices/soc0/images
    * Walking through modem.b12 file using `strings` is slow,
      It takes some seconds to finish
    * This new way is apparently way faster and more reliable
    
    Change-Id: I1fee1ebfc5f7e812c276086e26c126bcdd7a069c

--- rootdir/bin/init.baseband.sh
+++ rootdir/bin/init.baseband.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-baseband_str=$(strings /vendor/firmware_mnt/image/modem.b12 | grep "QC_IMAGE_VERSION_STRING=" | cut -c 25-)
+baseband_str=$(grep -Eo 'MPSS.*' /sys/devices/soc0/images)
 
 if [ ! -z $baseband_str ]; then
     setprop vendor.gsm.version.baseband $baseband_str
