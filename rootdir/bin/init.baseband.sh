7d9e736056373f0b8fda2816ac6fa9ce157da2e8
iusmac <iusico.maxim@libero.it>
2023-11-23 13:32:13 +0100

    rova: rootdir: Rework modem version string parsing
    Sometimes SoC info driver isn't ready yet and we end with an empty
    modem version string.
    
    Therefore bring back the old way of parsing using firmware modem file,
    but as improvement we scan the file only with grep, where the file is
    treated as text and zero bytes as newlines. We also stop reading the
    rest of the file after first match. This effectively emulates what
    'strings' did before, but is still faster taking less than a second to
    complete.
    
    This commit reverts ea75df0dff3ba8e8f690c4fada4f014871541fc4.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- rootdir/bin/init.baseband.sh
+++ rootdir/bin/init.baseband.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-baseband_str=$(grep -Eo 'MPSS.*' /sys/devices/soc0/images)
+baseband_str=$(grep -m1 -az 'QC_IMAGE_VERSION_STRING=' /vendor/firmware_mnt/image/modem.b12 | cut -c 25-)
 
 if [ ! -z $baseband_str ]; then
     setprop vendor.gsm.version.baseband $baseband_str
