e3e4c7e18f75b0b7897f9e47a5027010fcd7b053
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    rova: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- device.mk
+++ device.mk
@@ -411,6 +411,7 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio.c_shim@1.0 \
     android.hardware.radio.c_shim@1.1 \
     android.hardware.radio.c_shim@1.2 \
