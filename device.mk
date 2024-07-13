a00a7496a45fad355c8bbe9e030428674e764f4c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 10:26:46 +0800

    rova: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -142,9 +142,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.qcom
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.msm8937
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     libdisplayconfig \
