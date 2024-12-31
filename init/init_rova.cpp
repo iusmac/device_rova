41ed02e309c3a2c3ecc43d3a18841d591e7e6358
iusmac <iusico.maxim@libero.it>
2024-12-31 15:32:40 +0100

    rova: init: fix `heapgrowthlimit`
    heapgrowthlimit should be half of heapsize (512/2=256).
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- init/init_rova.cpp
+++ init/init_rova.cpp
@@ -208,7 +208,7 @@ void set_dalvik_heap_size()
     } else if (sys.totalram > 1024ull * 1024 * 1024) {
         // from - phone-xhdpi-2048-dalvik-heap.mk
         heapstartsize = "8m";
-        heapgrowthlimit = "192m";
+        heapgrowthlimit = "256m";
         heapsize = "512m";
         heaptargetutilization = "0.75";
         heapminfree = "512k";
