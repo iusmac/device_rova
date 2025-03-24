67f2cbeca22146c78117880d35ede94770e82724
iusmac <iusico.maxim@libero.it>
2025-03-24 17:57:42 +0100

    rova: Update DEX preopt boot image profile location
    They are now moved from {fwb}/config to {fwb}/boot and split into to
    files.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -462,7 +462,9 @@ PRODUCT_PACKAGES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
+    frameworks/base/boot/boot-image-profile.txt \
+    frameworks/base/boot/boot-image-profile-extra.txt
 
 # Reduce system image size by limiting debug info
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
