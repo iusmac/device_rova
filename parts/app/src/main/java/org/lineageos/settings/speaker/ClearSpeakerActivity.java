38fdf94276f826d8ab27fdf1149c69fcc0c1fe57
iusmac <iusico.maxim@libero.it>
2025-07-03 12:34:50 +0200

    rova: parts: Enable predictive back gesture
    * With Android 15, predictive back gesture is not longer behind the
    developer options. Apps can opt in to predictive back gesture fully or
    at the activity level.
    
    * While we're at it, remove the deprecated Activity.onBackPressed() method.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- parts/app/src/main/java/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/app/src/main/java/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -17,7 +17,6 @@
 package org.lineageos.settings.speaker;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
@@ -32,14 +31,5 @@ public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
                     com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                     new ClearSpeakerFragment()).commit();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
