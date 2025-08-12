df268cc5ad1d057e994734c0341964d9434559a5
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-13 01:23:54 +0800

    rova: libshim: Automated Android.bp conversion
    Change-Id: Ie3307ce6023ad6cea2a49877f28a7cdcc592f26e

--- libshim/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libgui_vendor
-LOCAL_MODULE := libwui
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
