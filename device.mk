1047f279fb4d91fe70ccce2554f37f3b58e313e5
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    rova: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -252,8 +252,10 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay-service.sdm \
     vendor.lineage.livedisplay@2.0-service-sysfs
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
 
 # Media
 PRODUCT_COPY_FILES += \
