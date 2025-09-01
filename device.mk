3c7741060215cd4b56346785fc858e480982b77d
iusmac <iusico.maxim@libero.it>
2025-09-01 23:35:01 +0200

    Revert "rova: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`"
    This reverts commit f44e09afaee0b9e55ad5266c0615043f9a569bd0.

--- device.mk
+++ device.mk
@@ -504,9 +504,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.xiaomi_rova
 
-# UFFD GC
-OVERRIDE_ENABLE_UFFD_GC := false
-
 # USB
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
