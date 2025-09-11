iusmac/device_rova

commit 0f6e0653253a6a7812125e793d6f427589ad8c39

[SQUASH] rova: qcom-caf: audio: Import upstream changes
* From https://github.com/LineageOS/android_hardware_qcom_audio/commits/lineage-22.2-caf-msm8953/

---

  commit e34a1136133a618741f74057491d03ef188ae887
  Author: Michael Bestas <mkbestas@lineageos.org>
  Date:   Thu Jan 16 10:07:30 2025 +0200

      hal: Convert audio extensions to blueprint

      Change-Id: I32ed6c427245877a978039ec793f15806d578cc2

  commit a602c07b219ad0d4ebae1aa3f4ba55f0e3624fbc
  Author: Ricardo Cerqueira <ricardo@cyngn.com>
  Date:   Mon Jun 20 00:41:47 2016 +0100

      ssr: Fix building with OSS materials

      Change-Id: I466816af5e1258f61401edf3eaf37ed7a862292e

  commit 7b9aafe51fd8676279de5c85fda771159903f128
  Author: Michael Bestas <mkbestas@lineageos.org>
  Date:   Thu Jan 16 11:02:48 2025 +0200

      hal: Convert primary HAL to blueprint

      Change-Id: Id369bcedf537addfbb55551874edb888538af13c

  commit 3e464ea393baa1957cd10ad7674f3915ccdbf379
  Author: Michael Bestas <mkbestas@lineageos.org>
  Date:   Thu Jan 16 11:02:45 2025 +0200

      hal: Remove unused libmaxxaudio extension

      Change-Id: Ib4138fe8c0121b4efb30997f4bf998dc3a68d834

  commit 88752b2d413f406469eec4db2bf1a61bb70ebe83
  Author: Michael Bestas <mkbestas@lineageos.org>
  Date:   Thu Jan 16 11:02:36 2025 +0200

      hal: Remove feature manager leftovers

      This has been removed since 2019.

      Change-Id: Ic76fa07b8649c7135afbbf10805854eddb937a0e

  commit b7657a083ed6e2a0a7d9a56e3258b180944827b7
  Author: danielml <daniel@danielml.dev>
  Date:   Wed Jun 25 20:10:53 2025 +0200

      hal: audio_extn: Add argument to pthread routines

      Change-Id: Ia932e0314d5d51e1eb7e4e1211f747e21caf0995
      Signed-off-by: danielml <daniel@danielml.dev>

  commit e804ce7d4dca8482add2b5ab40cc1df9b6f6d22b
  Author: synergydev <synergye@codefi.re>
  Date:   Sat Oct 19 22:28:40 2013 -0700

      voice_processing: do not use GNU old-style field designators

      Not using a GNU extension improves portability

qcom-caf/audio/hal/audio_extn/cirrus_playback.c
@@ -165,13 +165,13 @@ struct pcm_config pcm_config_cirrus_tx = {
 static struct cirrus_playback_session handle;
 
 #ifdef CIRRUS_FACTORY_CALIBRATION
-static void *audio_extn_cirrus_calibration_thread(void*);
+static void *audio_extn_cirrus_calibration_thread();
 #else
-static void *audio_extn_cirrus_config_thread(void*);
+static void *audio_extn_cirrus_config_thread();
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-static void *audio_extn_cirrus_failure_detect_thread(void*);
+static void *audio_extn_cirrus_failure_detect_thread();
 #endif
 
 void spkr_prot_init(void *adev, spkr_prot_init_config_t spkr_prot_init_config_val) {
@@ -476,7 +476,7 @@ exit:
     return ret;
 }
 
-static void *audio_extn_cirrus_calibration_thread(void*) {
+static void *audio_extn_cirrus_calibration_thread() {
     struct audio_device *adev = handle.adev_handle;
     struct audio_usecase *uc_info_rx = NULL;
     int ret = 0;
@@ -576,7 +576,7 @@ exit:
 }
 
 #else
-static void *audio_extn_cirrus_config_thread(void*) {
+static void *audio_extn_cirrus_config_thread() {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct cirrus_cal_result_t result;
@@ -659,7 +659,7 @@ exit:
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-void *audio_extn_cirrus_failure_detect_thread(void*) {
+void *audio_extn_cirrus_failure_detect_thread() {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct mixer_ctl *ctl = NULL;

qcom-caf/audio/hal/audio_extn/spkr_protection.c
@@ -1170,7 +1170,7 @@ static void spkr_calibrate_wait()
     pthread_mutex_unlock(&handle.cal_wait_cond_mutex);
 }
 
-static void* spkr_calibration_thread(void*)
+static void* spkr_calibration_thread()
 {
     unsigned long sec = 0;
     int t0;
@@ -1953,7 +1953,7 @@ static int get_spkr_prot_v_vali_param(int cal_fd __unused, int *status __unused,
 }
 #endif
 
-static void* spkr_v_vali_thread(void*)
+static void* spkr_v_vali_thread()
 {
     int ret = 0;
     struct audio_device *adev = handle.adev_handle;

qcom-caf/audio/visualizer/Android.bp
@@ -1,0 +1,41 @@
+//
+// SPDX-FileCopyrightText: 2013 The Android Open Source Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+cc_library_shared {
+    name: "libqcomvisualizer",
+    relative_install_path: "soundfx",
+    vendor: true,
+
+    srcs: [
+        "offload_visualizer.c"
+    ],
+
+    cflags: [
+        "-fvisibility=hidden",
+        "-Werror",
+        "-Wno-gnu-designator",
+    ],
+
+    include_dirs: [
+        "external/tinyalsa/include",
+        "system/media/audio_effects/include",
+    ],
+
+    header_libs: [
+        "libsystem_headers",
+        "libhardware_headers",
+    ],
+
+    shared_libs: [
+        "libcutils",
+        "libdl",
+        "liblog",
+        "libtinyalsa",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}

qcom-caf/audio/hal/Android.bp
@@ -1,0 +1,124 @@
+soong_config_module_type {
+    name: "target_board_platform_cc_library_shared",
+    module_type: "cc_library_shared",
+    config_namespace: "ANDROID",
+    value_variables: ["target_board_platform"],
+    properties: [
+        "name",
+    ],
+}
+
+target_board_platform_cc_library_shared {
+    name: "audio.primary.qcom",
+    soong_config_variables: {
+        target_board_platform: {
+            name: "audio.primary.%s",
+            conditions_default: {
+                name: "audio.primary.qcom",
+            },
+        },
+    },
+    relative_install_path: "hw",
+    vendor: true,
+    owner: "qti",
+
+    srcs: [
+        "acdb.c",
+        "audio_hw.c",
+        "audio_perf.cpp",
+        "platform_info.c",
+        "voice.c",
+        "audio_extn/audio_extn.c",
+        "audio_extn/audio_hidl.cpp",
+        "audio_extn/compress_in.c",
+        "audio_extn/device_utils.c",
+        "audio_extn/fm.c",
+        "audio_extn/keep_alive.c",
+        "audio_extn/source_track.c",
+        "audio_extn/usb.c",
+        "audio_extn/utils.c",
+        "msm8974/hw_info.c",
+        "msm8974/platform.c",
+        "voice_extn/compress_voip.c",
+        "voice_extn/voice_extn.c",
+    ] + select(soong_config_variable("qtiaudio", "feature_gef_support"), {
+        "true": ["audio_extn/gef.c"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_sound_trigger"), {
+        "true": ["audio_extn/soundtrigger.c"],
+        default: [],
+    }),
+
+    cflags: [
+        "-Werror",
+        "-D_GNU_SOURCE",
+        "-DHW_VARIANTS_ENABLED",
+    ] + select(soong_config_variable("qtiaudio", "feature_extended_compress_format"), {
+        "true": ["-DENABLE_EXTENDED_COMPRESS_FORMAT"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_gef_support"), {
+        "true": ["-DAUDIO_GENERIC_EFFECT_FRAMEWORK_ENABLED"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_gki"), {
+        "true": ["-DAUDIO_GKI_ENABLED"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_instance_id"), {
+        "true": ["-DINSTANCE_ID_ENABLED"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_sound_trigger"), {
+        "true": ["-DSOUND_TRIGGER_ENABLED"],
+        default: [],
+    }) + select(soong_config_variable("ANDROID", "target_board_platform"), {
+        "msm8937": [
+            "-DPLATFORM_MSM8937",
+            "-DMAX_TARGET_SPECIFIC_CHANNEL_CNT=8",
+            "-DSOUND_TRIGGER_PLATFORM_NAME=msm8937",
+        ],
+        "msm8953": [
+            "-DPLATFORM_MSM8953",
+            "-DMAX_TARGET_SPECIFIC_CHANNEL_CNT=8",
+            "-DSOUND_TRIGGER_PLATFORM_NAME=msm8953",
+        ],
+        default: [],
+    }),
+
+    local_include_dirs: [
+        "msm8974",
+        "audio_extn",
+        "voice_extn",
+    ],
+
+    include_dirs: [
+        "external/expat/lib",
+        "external/tinyalsa/include",
+        "external/tinycompress/include",
+        "system/media/audio_effects/include",
+        "system/media/audio_route/include",
+        "system/media/audio_utils/include",
+    ],
+
+    header_libs: [
+        "libhardware_headers",
+        "qti_kernel_headers",
+    ],
+
+    shared_libs: [
+        "android.hardware.power@1.2",
+        "libaudioroute",
+        "libaudioutils",
+        "libbase",
+        "libcutils",
+        "libdl",
+        "libexpat",
+        "libhidlbase",
+        "liblog",
+        "libprocessgroup",
+        "libtinyalsa",
+        "libtinycompress",
+        "libutils",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}

qcom-caf/audio/hal/audio_extn/Android.bp
@@ -1,0 +1,213 @@
+cc_defaults {
+    name: "qtiaudio_extn_defaults",
+
+    cflags: [
+        "-Werror",
+    ],
+
+    local_include_dirs: [
+        "../",
+        "../msm8974",
+    ],
+
+    include_dirs: [
+        "external/tinyalsa/include",
+        "external/tinycompress/include",
+        "system/media/audio_route/include",
+        "system/media/audio_utils/include",
+    ],
+
+    header_libs: [
+        "libhardware_headers",
+        "qti_kernel_headers",
+    ],
+
+    shared_libs: [
+        "libcutils",
+        "liblog",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}
+
+cc_library_shared {
+    name: "libsndmonitor",
+    defaults: ["qtiaudio_extn_defaults"],
+    owner: "third_party",
+    vendor: true,
+
+    srcs: [
+        "sndmonitor.c",
+    ],
+}
+
+cc_library_shared {
+    name: "libcomprcapture",
+    defaults: ["qtiaudio_extn_defaults"],
+    owner: "third_party",
+    vendor: true,
+
+    srcs: [
+        "compress_capture.c",
+    ],
+}
+
+cc_library_shared {
+    name: "libssrec",
+    defaults: ["qtiaudio_extn_defaults"],
+    vendor: true,
+
+    srcs: [
+        "ssr.c",
+        "device_utils.c",
+    ],
+
+    cflags: [
+        "-D_OSS",
+    ],
+
+    shared_libs: [
+        "libprocessgroup",
+        "libtinyalsa",
+    ],
+}
+
+cc_library_shared {
+    name: "libhdmiedid",
+    defaults: ["qtiaudio_extn_defaults"],
+    owner: "third_party",
+    vendor: true,
+
+    srcs: [
+        "edid.c",
+    ],
+}
+
+cc_library_shared {
+    name: "libspkrprot",
+    defaults: ["qtiaudio_extn_defaults"],
+    owner: "third_party",
+    vendor: true,
+
+    srcs: [
+        "spkr_protection.c"
+    ],
+
+    cflags: [
+        "-DSPKR_PROT_ENABLED",
+    ],
+
+    shared_libs: [
+        "libaudioroute",
+        "libtinyalsa",
+    ],
+}
+
+cc_library_shared {
+    name: "libcirrusspkrprot",
+    defaults: ["qtiaudio_extn_defaults"],
+    owner: "third_party",
+    vendor: true,
+
+    srcs: [
+        "cirrus_playback.c",
+    ],
+
+    cflags: [
+        "-DENABLE_CIRRUS_DETECTION",
+        "-DCIRRUS_FACTORY_CALIBRATION",
+    ],
+
+    shared_libs: [
+        "libaudioroute",
+        "libtinyalsa",
+    ],
+}
+
+cc_library_shared {
+    name: "liba2dpoffload",
+    defaults: ["qtiaudio_extn_defaults"],
+    vendor: true,
+
+    srcs: [
+        "a2dp.c",
+        "device_utils.c",
+    ],
+
+    shared_libs: [
+        "libtinyalsa",
+    ],
+}
+
+cc_library_shared {
+    name: "libexthwplugin",
+    defaults: ["qtiaudio_extn_defaults"],
+    vendor: true,
+
+    srcs: [
+        "ext_hw_plugin.c"
+    ],
+}
+
+cc_library_shared {
+    name: "libhfp",
+    defaults: ["qtiaudio_extn_defaults"],
+    vendor: true,
+
+    srcs: [
+        "hfp.c",
+        "device_utils.c",
+    ],
+
+    shared_libs: [
+        "libtinyalsa",
+    ],
+}
+
+cc_library_shared {
+    name: "libbatterylistener",
+    vendor: true,
+
+    srcs: [
+        "battery_listener.cpp",
+    ],
+
+    cflags: [
+        "-DDTSHD_PARSER_ENABLED",
+    ],
+
+    shared_libs: [
+        "android.hardware.health@1.0",
+        "android.hardware.health@2.0",
+        "android.hardware.power@1.2",
+        "libbase",
+        "libhidlbase",
+        "liblog",
+        "libutils",
+    ],
+
+    static_libs: [
+        "libhealthhalutils",
+    ],
+}
+
+cc_library_shared {
+    name: "libaudiozoom",
+    defaults: ["qtiaudio_extn_defaults"],
+    vendor: true,
+
+    srcs: [
+        "audiozoom.c",
+        "device_utils.c",
+    ],
+
+    include_dirs: [
+        "external/expat/lib",
+    ],
+
+    shared_libs: [
+        "libexpat",
+    ],
+}

qcom-caf/audio/post_proc/Android.bp
@@ -1,0 +1,99 @@
+cc_library_shared {
+    name: "libqcompostprocbundle",
+    relative_install_path: "soundfx",
+    vendor: true,
+    owner: "qti",
+
+    srcs: [
+        "bundle.c",
+        "equalizer.c",
+        "bass_boost.c",
+        "virtualizer.c",
+        "reverb.c",
+        "effect_api.c",
+        "effect_util.c",
+    ],
+
+    cflags: [
+        "-fvisibility=hidden",
+        "-Werror",
+        "-DDTS_EAGLE",
+    ] + select(soong_config_variable("qtiaudio", "feature_gki"), {
+        "true": ["-DAUDIO_GKI_ENABLED"],
+        default: [],
+    }) + select(soong_config_variable("qtiaudio", "feature_instance_id"), {
+        "true": ["-DINSTANCE_ID_ENABLED"],
+        default: [],
+    }),
+
+    include_dirs: [
+        "external/tinyalsa/include",
+        "system/media/audio_effects/include",
+    ],
+
+    header_libs: [
+        "libhardware_headers",
+        "libsystem_headers",
+        "qti_kernel_headers",
+    ],
+
+    shared_libs: [
+        "libcutils",
+        "libdl",
+        "liblog",
+        "libtinyalsa",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}
+
+cc_library_shared {
+    name: "libvolumelistener",
+    relative_install_path: "soundfx",
+    vendor: true,
+    owner: "qti",
+
+    srcs: [
+        "volume_listener.c"
+    ],
+
+    cflags: [
+        "-fvisibility=hidden",
+        "-Werror",
+    ] + select(soong_config_variable("ANDROID", "target_board_platform"), {
+        any @ flag_val: [
+            "-DLIB_AUDIO_HAL=/vendor/lib/hw/audio.primary." + flag_val + ".so",
+            "-DLIB64_AUDIO_HAL=/vendor/lib64/hw/audio.primary." + flag_val + ".so",
+        ],
+        default: [
+            "-DLIB_AUDIO_HAL=/vendor/lib/hw/audio.primary.qcom.so",
+            "-DLIB64_AUDIO_HAL=/vendor/lib64/hw/audio.primary.qcom.so",
+        ],
+    }),
+
+    include_dirs: [
+        "external/tinyalsa/include",
+        "external/tinycompress/include",
+        "hardware/qcom-caf/msm8953/audio/hal",
+        "hardware/qcom-caf/msm8953/audio/hal/audio_extn",
+        "system/media/audio_route/include",
+        "system/media/audio_utils/include",
+    ],
+
+    header_libs: [
+        "libhardware_headers",
+        "qti_kernel_headers",
+    ],
+
+    shared_libs: [
+        "libcutils",
+        "libdl",
+        "liblog",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}

qcom-caf/audio/voice_processing/Android.bp
@@ -1,0 +1,33 @@
+cc_library_shared {
+    name: "libqcomvoiceprocessing",
+    relative_install_path: "soundfx",
+    vendor: true,
+    owner: "qti",
+
+    srcs: [
+        "voice_processing.c",
+    ],
+
+    cflags: [
+        "-fvisibility=hidden",
+        "-Werror",
+    ],
+
+    include_dirs: [
+        "system/media/audio_effects/include",
+    ],
+
+    header_libs: [
+        "libhardware_headers",
+    ],
+
+    shared_libs: [
+        "libcutils",
+        "libdl",
+        "liblog",
+    ],
+
+    sanitize: {
+        integer_overflow: true,
+    },
+}

commit ae27be1ced008722523aa6aca3aa45d0b6f17b6c

rova: qcom-caf: audio: Fix compilation errors
* Regression introduced in 0f6e0653253a6a7812125e793d6f427589ad8c39.

---

device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:450:33: error: unused parameter 'arg' [-Werror,-Wunused-parameter]
  450 | void *capture_thread_loop(void *arg)
      |                                 ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:815:85: error: unused parameter 'size' [-Werror,-Wunused-parameter]
  815 | int visualizer_set_parameter(effect_context_t *context, effect_param_t *p, uint32_t size)
      |                                                                                     ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:948:79: error: unused parameter 'cmdSize' [-Werror,-Wunused-parameter]
  948 | int visualizer_command(effect_context_t * context, uint32_t cmdCode, uint32_t cmdSize,
      |                                                                               ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:949:15: error: unused parameter 'pCmdData' [-Werror,-Wunused-parameter]
  949 |         void *pCmdData, uint32_t *replySize, void *pReplyData)
      |               ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:1085:34: error: unused parameter 'sessionId' [-Werror,-Wunused-parameter]
 1085 |                          int32_t sessionId,
      |                                  ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:1206:40: error: unused parameter 'inBuffer' [-Werror,-Wunused-parameter]
 1206 |                        audio_buffer_t *inBuffer,
      |                                        ^
device/xiaomi/rova/qcom-caf/audio/visualizer/offload_visualizer.c:1207:40: error: unused parameter 'outBuffer' [-Werror,-Wunused-parameter]
 1207 |                        audio_buffer_t *outBuffer)
      |                                        ^
7 errors generated.

---

qcom-caf/audio/hal/audio_extn/cirrus_playback.c
@@ -165,13 +165,13 @@ struct pcm_config pcm_config_cirrus_tx = {
 static struct cirrus_playback_session handle;
 
 #ifdef CIRRUS_FACTORY_CALIBRATION
-static void *audio_extn_cirrus_calibration_thread();
+static void *audio_extn_cirrus_calibration_thread(void*);
 #else
-static void *audio_extn_cirrus_config_thread();
+static void *audio_extn_cirrus_config_thread(void*);
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-static void *audio_extn_cirrus_failure_detect_thread();
+static void *audio_extn_cirrus_failure_detect_thread(void*);
 #endif
 
 void spkr_prot_init(void *adev, spkr_prot_init_config_t spkr_prot_init_config_val) {
@@ -476,7 +476,7 @@ exit:
     return ret;
 }
 
-static void *audio_extn_cirrus_calibration_thread() {
+static void *audio_extn_cirrus_calibration_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct audio_usecase *uc_info_rx = NULL;
     int ret = 0;
@@ -576,7 +576,7 @@ exit:
 }
 
 #else
-static void *audio_extn_cirrus_config_thread() {
+static void *audio_extn_cirrus_config_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct cirrus_cal_result_t result;
@@ -659,7 +659,7 @@ exit:
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-void *audio_extn_cirrus_failure_detect_thread() {
+void *audio_extn_cirrus_failure_detect_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct mixer_ctl *ctl = NULL;

qcom-caf/audio/hal/audio_extn/spkr_protection.c
@@ -1170,7 +1170,7 @@ static void spkr_calibrate_wait()
     pthread_mutex_unlock(&handle.cal_wait_cond_mutex);
 }
 
-static void* spkr_calibration_thread()
+static void* spkr_calibration_thread(void*)
 {
     unsigned long sec = 0;
     int t0;
@@ -1953,7 +1953,7 @@ static int get_spkr_prot_v_vali_param(int cal_fd __unused, int *status __unused,
 }
 #endif
 
-static void* spkr_v_vali_thread()
+static void* spkr_v_vali_thread(void*)
 {
     int ret = 0;
     struct audio_device *adev = handle.adev_handle;

qcom-caf/audio/visualizer/Android.bp
@@ -14,8 +14,19 @@ cc_library_shared {
 
     cflags: [
         "-fvisibility=hidden",
+        "-Wall",
         "-Werror",
         "-Wno-gnu-designator",
+        "-Wno-shorten-64-to-32",
+        "-Wno-sign-compare",
+        "-Wno-tautological-compare",
+        "-Wno-typedef-redefinition",
+        "-Wno-unused-function",
+        "-Wno-unused-label",
+        "-Wno-unused-local-typedef",
+        "-Wno-unused-parameter",
+        "-Wno-unused-value",
+        "-Wno-unused-variable",
     ],
 
     include_dirs: [