808531b4620f994c97a0b399c28564c2e69ad19c
iusmac <iusico.maxim@libero.it>
2025-09-28 09:24:05 +0200

    rova: Drop `PRODUCT_SYSTEM_SERVER_COMPILER_FILTER`
    * Already set to "speed-profile" since Android 14.
    
      See https://source.android.com/docs/core/runtime/configure
    
        PRODUCT_SYSTEM_SERVER_COMPILER_FILTER
        This option specifies the compiler filter for system server.
    
        * (Android 14 and higher) If unspecified, the speed-profile compiler
          filter is used, or the speed compiler filter is used if a profile
          isn't provided.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -459,8 +459,7 @@ QCOM_SOONG_NAMESPACE := \
 PRODUCT_PACKAGES += \
     subsystem_state_notifier
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+# Reduce RAM and storage
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
     frameworks/base/boot/boot-image-profile.txt \
