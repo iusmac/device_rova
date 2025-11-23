dac955977157a0e1fdd9b136b7b73b9930ebaf5b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    rova: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -251,9 +251,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm \
     vendor.lineage.livedisplay-service.sysfs
 
-$(call soong_config_set,livedisplay_sdm,enable_dm,false)
-$(call soong_config_set,livedisplay_sysfs,enable_ce,true)
-$(call soong_config_set,livedisplay_sysfs,enable_re,true)
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set_bool,livedisplay_sysfs,enable_ce,true)
+$(call soong_config_set_bool,livedisplay_sysfs,enable_re,true)
 
 # Media
 PRODUCT_COPY_FILES += \
