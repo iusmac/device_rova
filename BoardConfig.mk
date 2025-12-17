7e85e11c437240459c5cac22f4a4748b5e59000c
iusmac <iusico.maxim@libero.it>
2025-12-17 15:16:28 +0100

    Revert "rova: Build KernelSU (KSU) kernel module with SUSFS support"
    This reverts commit 99a2c4ed68e225c40df8822c6cf8f0e9406148f5.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,8 +148,7 @@ TARGET_KERNEL_CONFIG += \
 endif
 TARGET_KERNEL_CONFIG += \
     vendor/feature/pocket-judge.config \
-    vendor/feature/net.config \
-    vendor/feature/ksu_susfs.config
+    vendor/feature/net.config
 TARGET_KERNEL_VERSION := 4.19
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1
